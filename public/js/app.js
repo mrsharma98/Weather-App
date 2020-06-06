c90a812be7f25135a4e18bf70f3dc5a570dbed74
Manish Sharma <mrsharma98@gmail.com>
2020-06-07 01:15:56 +0530

    Client-side JS file

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-console.log("Client side JS file is loaded...");
+fetch("http://localhost:3000/weather?address=mumbai").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
--- src/app.js
+++ src/app.js
@@ -51,22 +51,25 @@ app.get("/weather", (req, res) => {
     });
   }
 
-  geocode(req.query.address, (error, { latitude, longitude, location }) => {
-    if (error) {
-      return res.send({ error });
-    }
-    forecast(latitude, longitude, (error, forecastData) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({ error });
       }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
 
-      res.send({
-        forecast: forecastData,
-        location,
-        address: req.query.address,
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 app.get("/help/*", (req, res) => {
