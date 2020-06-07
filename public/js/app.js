b80b7d1f4223d0cee2a2be26a94fdcee696c52cb
Manish Sharma <mrsharma98@gmail.com>
2020-06-07 19:49:51 +0530

    final commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,12 @@
 // grabbing the location from form
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
 weatherForm.addEventListener("sumbit", (e) => {
   e.preventDefault();
-  // this will prevent page to reload immediately after submitting our location
+  // this will prevent page to get reload immediately after submitting our location
 
   const location = search.value;
   // search.value extracts the input value
@@ -12,14 +14,20 @@ weatherForm.addEventListener("sumbit", (e) => {
   //console.log(location);
   // Now using this location we need to generate url
 
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
   fetch("http://localhost:3000/weather?address=" + location).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          //console.log(data.error);
+          messageOne.textContent = data.error;
         } else {
-          console.log(data.location);
-          console.log(data.forecast);
+          //console.log(data.location);
+          //console.log(data.forecast);
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
         }
       });
     }
